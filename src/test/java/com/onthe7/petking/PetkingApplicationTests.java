663e0873a1279b401397cbc283a173ff7c5e1f5d
hcgo97 <hcgo_o@naver.com>
2022-07-09 13:11:36 +0900

    Initial commit

---
+++ src/test/java/com/onthe7/petking/PetkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onthe7.petking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
