2ee3ccf5577dbb761f46df2a36ddc2238441c0f5
hcgo97 <hcgo_o@naver.com>
2022-07-10 14:19:55 +0900

    #3 공통 모듈 추가
    - ErrorCode 종류 추가
    - CLIENT_ERROR(400), AUTH_FAILED(401), NO_PERMISSION(403) 추가

--- src/main/java/com/onthe7/petking/common/enums/ErrorCode.java
+++ src/main/java/com/onthe7/petking/common/enums/ErrorCode.java
@@ -8,9 +8,18 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자 입니다"),
+    // 400
+    CLIENT_ERROR(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
-    COMMON_SYSTEM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "오류가 발생했습니다");
+    // 404
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+
+    // 401, 403
+    AUTH_FAILED(HttpStatus.UNAUTHORIZED, "잘못된 접근입니다."),
+    NO_PERMISSION(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+
+    // 500
+    COMMON_SYSTEM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
